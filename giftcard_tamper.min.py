7bce4a8843586bfe406ba2b72171ec8ae2b046f0
team6-hvg <bogesz02@yahoo.com>
2016-11-11 17:50:30 +0100

    secound day end

--- giftcard_tamper.min.py
+++ giftcard_tamper.min.py
@@ -1,7 +1,13 @@
 import base64
 
-giftcard = "HZvbp1XQ7JpKXeCWLGT/ndoInD6H7S1j2uaW74WCv7tLAqM="
+#giftcard = "HZvbp1XQ7JpKXeCWLGT/ndoInD6H7S1j2uaW74WCv7tLAqM="
+giftcard = "svdWSiyFmTq2vob939piIuzqic7Ht6tQRjz3ouXXRMQPzEZd"
 cardbytes = bytearray(base64.standard_b64decode(giftcard))
 print(map(hex, cardbytes))
+print(base64.standard_b64encode(cardbytes))
 
+#cardbytes[32] = cardbytes[32] ^ ord("1") ^ ord("9")
+cardbytes[33] = cardbytes[33] ^ ord("1") ^ ord("9")
+
+print(map(hex, cardbytes))
 print(base64.standard_b64encode(cardbytes))
